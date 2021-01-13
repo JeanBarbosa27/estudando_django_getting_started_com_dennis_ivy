c63ae1bd3e9df945c77b5adf09d8bbd255250bdd
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-01-13 18:36:26 -0300

    Passa contexto para as páginas Do dashboard e de produtos

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+from accounts.models import *
+
 
 def dashboard(request):
-    return render(request, 'pages/dashboard.html')
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+    total_orders = orders.count()
+    total_pending_orders = orders.filter(status='Pending').count()
+    total_delivered_orders = orders.filter(status='Delivered').count()
+
+    context = {
+        'customers': customers,
+        'orders': orders,
+        'total_orders': total_orders,
+        'total_pending_orders': total_pending_orders,
+        'total_delivered_orders': total_delivered_orders,
+    }
+
+    return render(request, 'pages/dashboard.html', context)
 
 def products(request):
-    return render(request, 'pages/products.html')
+    products = Product.objects.all()
+    return render(request, 'pages/products.html', {'products': products})
 
 def customers(request):
     return render(request, 'pages/customers.html')
