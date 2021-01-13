5178a2d156192332c22f1eaea260ce0726b10b2b
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-01-13 10:43:59 -0300

    Cria OneToMany Relationship

--- accounts/models/order.py
+++ accounts/models/order.py
@@ -1,4 +1,6 @@
 from django.db import models
+from .customer import Customer
+from .product import Product
 
 class Order(models.Model):
     STATUS = (
@@ -7,8 +9,7 @@ class Order(models.Model):
         ('Delivered', 'Delivered'),
     )
 
-    #customer =
-    #product =
-
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
