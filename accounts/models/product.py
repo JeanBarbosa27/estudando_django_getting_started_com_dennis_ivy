fb869fade8e48542158478300ee9ea6d39497adc
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-01-13 10:44:24 -0300

    Cria ManyToMany Relationship

--- accounts/models/product.py
+++ accounts/models/product.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .tag import Tag
 
 class Product(models.Model):
     CATEGORIES = (
@@ -9,7 +10,8 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORIES)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    tag = models.ManyToManyField(Tag)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
