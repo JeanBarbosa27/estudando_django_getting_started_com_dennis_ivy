106e29e6aaf381cc927dac9ae59910c9f9116168
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-01-13 08:08:20 -0300

    Configura rotas e define templates de accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def dashboard(request):
+    return render(request, 'pages/dashboard.html')
+
+def products(request):
+    return render(request, 'pages/products.html')
+
+def customers(request):
+    return render(request, 'pages/customers.html')
