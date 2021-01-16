fab0beffd20d25d1313afa0be78cad085aa779c7
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-01-16 08:30:58 -0300

    Cria formulário para página de perfil

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-from accounts.models import Order
+from accounts.models import Order, Customer
 
 
 class OrderForm(ModelForm):
@@ -15,3 +15,10 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+        exclude = ['user']
