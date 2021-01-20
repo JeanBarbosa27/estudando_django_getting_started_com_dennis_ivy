2618a0f461937163b40b06193cc1b888b2066e22
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-01-20 07:49:40 -0300

    Configura banco de dados postgres local

--- core/settings.py
+++ core/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crm_dennis_ivy',
+        'USER': 'django_of_crm_dennis_ivy',
+        'PASSWORD': '123@Mudar',
+        'HOST': 'localhost',
+        'PORT': '5434'
     }
 }
 
